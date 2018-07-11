1fe31480df80d5aedbab9e8958d2642148c3ccb3
aogle1 <aogle1@luc.edu>
2018-07-11 00:09:10 -0500

    Lab-6-AO

--- src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
+++ src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
@@ -17,38 +17,50 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
   private final E[] data;
 
   // TODO why do we need an explicit constructor?
+  // You are assigning a capacity to a fixed array queue.
 
-  @SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
   public FixedArrayQueue(final int capacity) {
     this.capacity = capacity;
     this.data = (E[]) new Object[capacity];
     this.size = 0;
     this.front = 0;
     this.rear = capacity - 1;
+
   }
 
   @Override
   public boolean offer(final E obj) {
-    // TODO
-    return false;
+    //cant reallocate what does this method do when you cant
+        data[rear] = obj;
+    if (size == capacity){
+        return false;
+    }
+      this.rear = (this.rear + 1)%this.capacity;
+      this.data[this.data] = item;
+      this.size = this.size + 1;
+      System.out.println(item+ " enqueued to queue");
+
   }
 
   @Override
   public E peek() {
-    // TODO
+    if (size == 0)
     return null;
+    else
+        return data[front];
   }
 
   @Override
   public E poll() {
-    // TODO
+    if (size == 0){
     return null;
   }
+  E result = data[front]
 
   @Override
-  public boolean isEmpty() {
-    // TODO
-    return true;
+  public boolean isEmpty(); {
+        return this.size == 0;
   }
 
   @Override
@@ -58,7 +70,15 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
 
   @Override
   public List<E> asList() {
-    // TODO implement using an ArrayList preallocated with the right size
-    return Arrays.asList();
+      List<E> queueList = new ArrayList<>(this.capacity);
+      for (int i = front; i )
+      while (front != null) {
+          queueList.add(front);
+          front = front;
+      }
+      return queueList;
+
+      return Arrays.asList();
   }
 }
+// TODO implement using an ArrayList preallocated with the right size
